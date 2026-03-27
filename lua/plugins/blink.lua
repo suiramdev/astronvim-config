fbbc16865e2502822affac62002fdc70476e3d7e
suiramdev <me@suiram.dev>
2026-03-27 10:33:01 +0100

    Change of cursor-tab plugin

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,12 +1,12 @@
 ---@type LazySpec
--- Tab = next item; Enter = accept LSP/completion (blink). S-Tab = AI accept (cursor-tab). C-p = previous item.
+-- Tab = AI (Cursor) only; Enter = accept LSP/completion (blink). S-Tab = previous item.
 return {
   "Saghen/blink.cmp",
   opts = function(_, opts)
     opts.keymap = vim.tbl_extend("force", opts.keymap or {}, {
-      ["<CR>"] = { "accept", "fallback" },  -- Enter: accept LSP/completion suggestion
-      ["<C-p>"] = { "select_prev", "fallback" },  -- Ctrl-p: previous item (S-Tab reserved for AI accept)
-      ["<S-Tab>"] = false,  -- disable: reserved for cursor-tab AI accept (nil is not merged by pairs())
+      ["<CR>"] = { "accept", "fallback" }, -- Enter: accept LSP/completion suggestion
+      ["<S-Tab>"] = { "select_prev", "fallback" },
+      ["<Tab>"] = false,
     })
     return opts
   end,
--- lua/plugins/cursor-tab.lua
+++ lua/plugins/cursor-tab.lua
@@ -1,20 +1,4 @@
 return {
-  "bengu3/cursor-tab.nvim",
-  config = function()
-    local cursor_tab = require "cursor-tab"
-
-    cursor_tab.setup()
-
-    -- Remove plugin's default <Tab> mapping so we can use Shift+Tab for AI accept
-    pcall(vim.keymap.del, "i", "<Tab>")
-
-    -- Use <S-Tab> (Shift+Tab) to accept AI suggestions only; do nothing when no suggestion
-    -- so blink's completion/snippet doesn't get triggered
-    vim.keymap.set("i", "<S-Tab>", function()
-      if cursor_tab.accept_suggestion() then
-        return ""
-      end
-      return ""  -- consume key when no AI suggestion (don't pass through to blink)
-    end, { expr = true, noremap = false, silent = true })
-  end,
+  "0xr3ngar/cursor-tab.nvim",
+  config = function() require("cursor-tab").setup() end,
 }
